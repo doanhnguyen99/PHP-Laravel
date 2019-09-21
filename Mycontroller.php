b9925ed54ee610814b9630fa84b1b39179f3b89b
doanhnguyen99 <doanh080199@gmail.com>
2019-09-21 15:56:28 +0700

    Add file Readme

---
+++ Mycontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Mycontroller extends Controller
+{
+    public function Xinchao()
+    {
+        echo 'Hello Controller';
+    }
+}
